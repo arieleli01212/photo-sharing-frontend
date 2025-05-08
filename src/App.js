5464d14cd7169daf0e6b00054f88252baf02d156
ariel elishayev <ardevops19@gmail.com>
2025-05-08 21:20:31 +0300

    need to fix fetch after upload

--- src/App.js
+++ src/App.js
@@ -8,13 +8,24 @@ import { Gallery } from'./components/Gallery/Gallery';
 
 function App() {
   const [imageCount, setImageCount] = useState(0);
+  const [guestCount, setGuestCount] = useState(0);
+  const [uploadedImages, setUploadedImages] = useState(0);
+
 
   return (
     <div>
       <Profile 
         imageCount={imageCount}
+        guestCount={guestCount}
+        setImageCount={setImageCount}
+        setUploadedImages={setUploadedImages} 
+        fetchImages={() => {
+          // Trigger the gallery sync from here
+          document.querySelector('gallery').fetchImages();
+        }}
       />
       <Gallery 
+        uploadedImages={uploadedImages}  // Pass uploadedImages to Gallery
         setImageCount={setImageCount} 
       />
     </div>
